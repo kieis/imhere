5d8a74222db5841a915515b01c208930cd236125
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-06-09 12:01:35 -0300

    feat: added status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
+import { StatusBar } from "react-native";
 import Home from "./src/screens/Home";
+import { Fragment } from "react";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Fragment>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </Fragment>
+  );
 }
